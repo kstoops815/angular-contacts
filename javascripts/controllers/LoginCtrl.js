12137a6edfa34941150414bf386f38ffde8983dc
Kelly Stoops <kstoops815@gmail.com>
2017-11-14 23:59:26 -0600

    login working and added contacts.json

--- javascripts/controllers/LoginCtrl.js
+++ javascripts/controllers/LoginCtrl.js
@@ -1,8 +1,8 @@
 "use strict";
 
-app.controller("LoginCtrl", function($location, $rootScope, $scope, AuthService){
+app.controller("LoginCtrl", function($location, $rootScope, $scope, LoginService){
 	$scope.authenticate = () => {
-		AuthService.authenticateGoogle().then((result) => {
+		LoginService.authenticateGoogle().then((result) => {
 			$rootScope.uid = result.user.uid;
 			$scope.$apply(() => {
 				$location.url("/search");
